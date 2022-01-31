7d426199e07b996ce7714ab5a401ba8c72ef8293
LindoelGomes <lindoelgomesdamasceno@gmail.com>
2022-01-31 14:54:00 -0300

    v3.0
    teste

--- app.js
+++ app.js
@@ -1,36 +1,32 @@
 function novanota() {
+
+    let listas = document.querySelector('#listas-conteiner')
     let card = document.querySelector('.lista-card')
-    let botao = document.querySelector('#botao')
-    let cards = document.querySelector('.nova')
     let titulo_dado = document.querySelector('.titulo').value
     let texto_dado = document.querySelector('#texto').value
+    let botao = document.querySelector('#botao')
+    
     card.style.display = 'block'
-
-    let b = botao.getAttribute('src')
     
     if(botao.getAttribute('src') == "icons/mais.svg") {
         botao.setAttribute('src', 'icons/salvar.svg')
     } else {
-        
+        //Criando elementos do card.
+        let nova = document.createElement('div')
         let titulo = document.createElement('h1')
         let texto = document.createElement('p')
         
+        //inserindo dados nos elementos criados
         titulo.textContent = titulo_dado
         texto.textContent = texto_dado
         
-        let listas = document.querySelector('#listas-conteiner')
-        let nova = document.createElement('div')
-        let edit = document.createElement('section')
-        
+        //Deixando o conteudo visivel.
         nova.appendChild(titulo)
         nova.appendChild(texto)
         nova.setAttribute('class', 'nova')
         listas.appendChild(nova)
-        
-        
+
         botao.setAttribute('src', 'icons/mais.svg')
         card.style.display = 'none'
-        texto_dado.value = ''
-        titulo_dado.value = ''
     }
 }
